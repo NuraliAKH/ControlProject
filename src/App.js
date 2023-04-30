8a1a8120fb4a57e16a0ea13227d4802910dc3168
odilbe19 <odilodilbe@gmail.com>
2023-04-30 17:44:48 +0500

    some changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import './App.css';
-<<<<<<< HEAD
 import Main from './components/Header/Header';
 import Scrole_title from './components/Scrole/Scrole_title';
 import Encient from './components/Encient/Encient';
-function App() {
-  return (
-    <div className="App">
-      <Encient/>
-=======
 import Servises from './components/Servises/Servises'
 import Contact from './components/Contact/Contact';
 import Footer from './components/Footer/Footer';
-
 function App() {
   return (
     <div className="App">
+      <Main/>
+      <Scrole_title/>
+      <Encient/>
       <Servises/>
       <Contact/>
       <Footer/>
->>>>>>> 536c8ead8870a119ce931e88cd637304eea4d240
     </div>
   );
 }
