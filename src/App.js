75b3d44aaa083b76e479e16cbf72158eec6965aa
odilbe19 <odilodilbe@gmail.com>
2023-05-02 18:02:27 +0500

    Odil

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import Encient from './components/Encient/Encient';
 import Servises from './components/Servises/Servises'
 import Contact from './components/Contact/Contact';
 import Footer from './components/Footer/Footer';
-import Tours from './components/Tours/Tours'
+import Tours from './components/Tours/Tours';
+
 function App() {
   return (
     <div className="App g-0">
